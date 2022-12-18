26b680a4a93bf2819b97dfb6c206073c1104326c
Ivan Matias <ivanmatias1331@gmail.com>
2022-12-18 05:16:56 -0800

    Implemented error system for adding new pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django import forms
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 import markdown2
 import re
 
 from . import util
 
 markdowner = markdown2.Markdown()
+
+class NewEntryForm(forms.Form):
+    entry = forms.CharField(label="New Entry")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -40,13 +45,21 @@ def search(request):
     
 def add(request):
     if request.method == "POST":
-        entry_check = request.POST.get("new_entry")
+        form = NewEntryForm(request.POST)
         current_entries = util.list_entries()
         
-        for entry in current_entries:
-            if entry_check.lower() == entry.lower():
-                return HttpResponse("This already exists")
-        util.save_entry(entry_check, 'No content inserted yet...')
-        return render(request, "encyclopedia/index.html")
+        if form.is_valid():
+            entry_form = form.cleaned_data["entry"]
+            for entry in current_entries:
+                if entry.lower() == entry_form.lower():
+                    return render(request, "encyclopedia/add.html", {
+                        "form":form,
+                        "error_message":f"The entry {entry_form} already exists!"
+                    })
+            util.save_entry(entry_form, 'No content inserted yet...')
+            return HttpResponseRedirect(reverse("encyclopedia:index"))
             
-    return render(request, "encyclopedia/add.html")
+    return render(request, "encyclopedia/add.html", {
+        "form": NewEntryForm(),
+        "error_message":""
+    })
